1cfa74a2a0ecced3ca88b68fcc665b2941f2e0af
mike881205 <mike881205@gmail.com>
2022-01-24 20:29:50 -0500

    updated shuffle and build functions, updated image array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import Stage from "./Components/Stage";
 import Slot from '../src/Components/Slot';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const images = ["img1", "img2", "img3", "img4", "img5", "img6"]
+
+const blankArr = Array.from(Array(5).fill(
+  {
+    number: null,
+    held: false,
+  }
+));
+
+const getRandNum = () => {
+  const min = Math.ceil(1);
+  const max = Math.floor(6);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class App extends Component {
 
   state = {
@@ -10,24 +25,45 @@ class App extends Component {
   };
 
   componentDidMount() {
-    const numArr = Array.from(Array(5).fill(''))
 
-    const slotsArr = numArr.map((slot, i) => {
-      slot =
-        <Slot
-          key={i}
-          num={i + 1}
-        />
+    this.setState({ slots: this.shuffle(blankArr) })
+  }
 
-      return slot;
+
+
+  shuffle = slots => {
+    const newSlots = slots.map(slot => {
+      let newSlot = {};
+      if (!slot.held) {
+        newSlot.number = getRandNum();
+      };
+      newSlot.held = slot.held;
+      return newSlot;
     });
 
-    this.setState({ slots: slotsArr })
-  }
+    return this.buildSlots(newSlots);
+  };
+
+  buildSlots = slots => {
+    const newSlots = slots.map((slot, i) => {
+      const newSlot =
+        <Slot
+          key={i}
+          number={slot.number}
+          image={images[slot.number - 1]}
+          held={slot.held}
+          click={null}
+        // click={this.holdSlot}
+        />;
+
+      return newSlot;
+    });
+    return newSlots;
+  };
 
   render() {
     return (
-      <div className="App">
+      <div className="App" align="center">
         {
           this.state.slots ?
             <Stage slots={this.state.slots} />
--- src/Components/Slot/index.js
+++ src/Components/Slot/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button'
 
-const Slot = ({num}) => {
+const Slot = ({ number, image, held, click }) => {
     return (
-        <Col>
-            <div className="slot">
-                {`Slot ${num}`}
-            </div>
+        <Col className="slot">
+            <Row>
+                <Col>
+                    {image}
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {number}
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button
+                        variant={!held ? "outline-warning" : "success"}
+                        onClick={e => click(e)}
+                    >
+                        {!held ? "Hold" : "Held"}
+                    </Button>{' '}
+                </Col>
+            </Row>
         </Col>
     )
 };
